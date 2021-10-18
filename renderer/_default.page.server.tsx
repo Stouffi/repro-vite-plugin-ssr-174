8e55f257ad3f0a16fe569e2ff95a89c831f663fd
Stouffi <765497+Stouffi@users.noreply.github.com>
2021-10-18 17:21:37 +0200

    Adds onBeforeRender which returns the full context.

--- renderer/_default.page.server.tsx
+++ renderer/_default.page.server.tsx
@@ -1,27 +1,32 @@
-import ReactDOMServer from "react-dom/server";
-import React from "react";
-import { PageWrapper } from "./PageWrapper";
-import { escapeInject, dangerouslySkipEscape } from "vite-plugin-ssr";
-import logoUrl from "./logo.svg";
-import type { PageContext } from "./types";
-import type { PageContextBuiltIn } from "vite-plugin-ssr";
+import ReactDOMServer from "react-dom/server"
+import React from "react"
+import { PageWrapper } from "./PageWrapper"
+import { escapeInject, dangerouslySkipEscape } from "vite-plugin-ssr"
+import logoUrl from "./logo.svg"
+import type { PageContext } from "./types"
+import type { PageContextBuiltIn } from "vite-plugin-ssr"
 
-export { render };
+export { render }
 // See https://vite-plugin-ssr.com/data-fetching
-export const passToClient = ["pageProps", "urlPathname"];
+export const passToClient = ["pageProps", "urlPathname"]
+
+
+export const onBeforeRender = async (ctx: PageContextBuiltIn) => {
+  return { pageContext: ctx }
+}
 
 async function render(pageContext: PageContextBuiltIn & PageContext) {
-  const { Page, pageProps } = pageContext;
+  const { Page, pageProps } = pageContext
   const pageHtml = ReactDOMServer.renderToString(
     <PageWrapper pageContext={pageContext}>
       <Page {...pageProps} />
     </PageWrapper>
-  );
+  )
 
   // See https://vite-plugin-ssr.com/html-head
-  const { documentProps } = pageContext;
-  const title = (documentProps && documentProps.title) || "Vite SSR app";
-  const desc = (documentProps && documentProps.description) || "App using Vite + vite-plugin-ssr";
+  const { documentProps } = pageContext
+  const title = (documentProps && documentProps.title) || "Vite SSR app"
+  const desc = (documentProps && documentProps.description) || "App using Vite + vite-plugin-ssr"
 
   const documentHtml = escapeInject`<!DOCTYPE html>
     <html lang="en">
@@ -35,12 +40,12 @@ async function render(pageContext: PageContextBuiltIn & PageContext) {
       <body>
         <div id="page-view">${dangerouslySkipEscape(pageHtml)}</div>
       </body>
-    </html>`;
+    </html>`
 
   return {
     documentHtml,
     pageContext: {
       // We can add some `pageContext` here, which is useful if we want to do page redirection https://vite-plugin-ssr.com/page-redirection
     },
-  };
+  }
 }
